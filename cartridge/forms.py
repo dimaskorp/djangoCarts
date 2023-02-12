9affe53caca078bd777ab204c9e26cda6137cba2
dimaskorp <52814195+dimaskorp@users.noreply.github.com>
2023-02-12 22:17:41 +0400

    Update forms.py

--- cartridge/forms.py
+++ cartridge/forms.py
@@ -1,103 +1,117 @@
-{% extends 'cartridge/base.html' %}
+from .models import Cartridges, Manufacturer, NameСartridge, Placements
+from django.forms import ModelForm, TextInput, Textarea, Select, NumberInput, DateTimeInput
 
-{% block title %}Смена местоположения картриджей с помощью сканера штрих-кодов{% endblock %}
 
-{% block submenu %}Картриджи / Смена местоположения картриджей с помощью сканера штрих-кодов{% endblock %}
+class ManufacturerForm(ModelForm):  # форма Производитель
+    class Meta:
+        model = Manufacturer
+        fields = ['manufacturer', 'comments']
+        widgets = {
+            'manufacturer': TextInput(attrs={
+                'name': 'name',
+                'maxlength': "256",
+                'required': "",
+                'id': "id_name"
+            }),
+            'comments': Textarea(attrs={
+                'name': 'comment',
+                'cols': '40',
+                'rows': '10',
+                'id': "id_comment"
+            })
+        }
 
-{% block content %}
-    <div class="row">
-        <div class="large-4 columns" style="display: {{ display }}">
-            <ul class="error_msg" style="display: {{ display }}">
-                <li class="success">{{ message }}</li>
-            </ul>
-        </div>
-    </div>
-    <div class="row">
-        <div class="large-5 columns">
-            <form action="{% url 'massive_change_room' %}" method="get">
-                <div class="row">
-                    <div class="large-12 columns">
-                        <label>Отсканированный штрихкод</label>
-                        {#                       {{ form.barcode }}#}
-                    </div>
-                </div>
-                <div class="marginTop"></div>
-                <div class="row">
-                    <div class="large-12 columns">
-                        <label>Ввести номер картриджа вручную</label>
-                        <div class="input_row">
-                            {#                            {{ form.barcode }}#}
-                            {#                            <input type="search" name="barcode" id="id_barcodeNumber" class="manual_number" autofocus="autofocus" onkeyup="return lengthBarcode()" required="">#}
-                            <input class="manual_number" type="search" autofocus="autofocus" aria-label="Search"
-                                   name="search">
-                            <script>
-                                function lengthBarcode() {
-                                    var col = document.getElementById("id_barcodeNumber").value.length
-                                    if (col === 0)
-                                        document.getElementById('id_placeNumber').disabled = true;
-                                    if (col === 13)
-                                        document.getElementById('id_placeNumber').disabled = false;
-                                    document.getElementById('id_placeNumber').focus();
-                                    return true;
-                                }
-                            </script>
-                            <button class="button small-move" type="submit">Поиск</button>
-                            {#                            <a title="Найти по номеру" type="submit" class="button no_follow add-session-move-barcode search-zoom" href=""></a>#}
 
-                        </div>
-                    </div>
-                </div>
-            </form>
-            <form action="{% url 'massive_change_room' %}" method="post">
-                {% csrf_token %}
-                <div class="marginTop"></div>
-                <div class="row">
-                    <div class="large-12 columns">
-                        <label class="required">Помещение</label>
-                        {#                        {{ form_place.barcode }}#}
-                        <a class="dj_button" href="{% url 'tree_list' %}" title="Добавить название помещения"></a>
+class PlacementsForm(ModelForm):  # форма Помещения
+    class Meta:
+        model = Placements
+        fields = ['barcode', 'name', 'branch']
+        widgets = {
+            'name': TextInput(attrs={
+                'required': "True",
+            }),
+            'branch': TextInput(attrs={
+                'name': 'branch',
+            }),
+            'barcode': NumberInput(attrs={
+                'name': 'number_barcode',
+                'id': "id_barcode"
+            })
+        }
 
-                        <div class="form_error_text cart_sklad_error">Поле должно быть выбрано</div>
-                    </div>
-                </div>
 
-                <div class="marginTop55"></div>
-                <div class="row">
-                    <div class="large-12 columns">
-                        <a class="button no_follow set_focus small-focus">Сфокусировать</a>
-                        <button class="button small-move" type="submit">Передать</button>
-                        {#                        <a class="button small-move" href="">Переместить</a>&nbsp;&nbsp;&nbsp;#}
-                        <img class="spinner" src="static/img/loader.gif">
-                    </div>
-                </div>
-            </form>
-        </div>
-        <div class="large-5 columns">
-            <div class="marginTop wrapper_add_scanner_items" style="display:block">
-                <a class="button remove-move-item no_follow small-delete-row">Удалить элемент</a>
-                <a class="button clear-move-session no_follow small-clear">Очистить сессию</a>
-                <span class="count_objects_wrapper">Кол-во:
-                    <span class="count_objects">0</span>
-                </span>
-                <img class="session_spinner" src="static/img/loader.gif">
-            </div>
-            <div class="marginTop"></div>
-            <div class="session-wrapper">
-                <div class="p_checkboxes cart_item_${pk}">
+class CartridgesForm(ModelForm):  # Картриджи
+    class Meta:
+        model = Cartridges
+        fields = ['barcode', 'cartName', 'placeName', 'manufacturerName', 'date']
+        widgets = {
+            'barcode': NumberInput(attrs={
+                'id': "id_manualNumber",
+                'style': "width: calc(100% - 38px);"
+            }),
+            'cartName': Select(attrs={
+                'id': "id_cartName",
+                'required': "True",
+                'class': 'select-selection--single',
+                'style': "width: calc(100% - 38px);"
+            }),
+            'placeName': Select(attrs={
+                'id': 'id_place',
+                'required': "True",
+                'class': 'select-selection--single',
+                'style': "width: calc(100% - 38px);"
+            }),
+            'manufacturerName': Select(attrs={
+                'id': 'id_manufacturer',
+                'required': "True",
+                'class': 'select-selection--single',
+                'style': "width: calc(100% - 38px);"
+            }),
+            'date': DateTimeInput(attrs={
+                'id': 'datetimepicker4',
+                'required': "True"
+            })
+        }
 
-                    {% for cart in carts %}
-                        <p>{{ forloop.counter }}. {{ cart.barcode }}</p>
-                        <p>{{ cart.cartName }}</p>
-                        <p>{{ cart.placeName }}</p>
-                    {% endfor %}
-                    {#                        <input class="checkbox" type='checkbox' name='cart_number' value='${pk}'>#}
-                    {#                        {{ queryset_form_cart }}&nbsp;&nbsp;&nbsp;{{ queryset_form_cart }}&nbsp;&nbsp;&nbsp;#}
-                    {#                        <span class="ballon">{{ queryset_form_cart }}</span>#}
 
-                    <hr/>
-                </div>
-            </div>
-        </div>
-        <div class="large-2 columns"></div>
-    </div>
-{% endblock %}
+class PlaceUpdateForm(ModelForm):  # Картриджи
+    class Meta:
+        model = Cartridges
+        fields = ['barcode', 'placeName']
+        widgets = {
+            'barcode': NumberInput(attrs={
+                'id': "id_barcodeNumber",
+                'class': 'manual_number',
+
+            }),
+            'barcode_manual': NumberInput(attrs={
+                'id': "id_manualNumber",
+                'class': 'manual_number',
+                # 'style': "width: calc(100% - 38px);"
+            }),
+            'placeName': NumberInput(attrs={
+                'id': 'id_placeNumber',
+                'required': "True",
+                'class': 'place_number',
+                'disabled': 'True',
+                'style': "width: calc(100% - 38px);"
+            })
+        }
+
+
+class NameСartridgeForm(ModelForm):  # форма Названия картриджей
+    class Meta:
+        model = NameСartridge
+        fields = ['name', 'comments']
+        widgets = {
+            'name': TextInput(attrs={
+                'required': "True",
+                'id': "id_cart_name"
+            }),
+            'comments': Textarea(attrs={
+                'name': 'comment',
+                'cols': '40',
+                'rows': '10',
+                'id': "id_comment"
+            })
+        }
